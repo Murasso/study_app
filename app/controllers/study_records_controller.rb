0da6d755ad01c92a77e690ca3b231e4a1e165705
Murasso <sou04136@gmail.com>
2023-11-05 07:26:54 +0000

    Start

---
+++ app/controllers/study_records_controller.rb
@@ -0,0 +1,22 @@
+class StudyRecordsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @study_record = StudyRecord.new
+  end
+  
+  def create
+    @study_record = StudyRecord.new(study_record_params)
+    @study_record.user_id = current_user.id
+    @study_record.save
+    redirect_to study_records_path
+  end
+  def show
+  end
+  private
+
+  def study_record_params
+    params.require(:study_record).permit(:title, :body, :num_of_session)
+  end
+end
---
+++ db/migrate/20231104064815_create_study_records.rb
@@ -0,0 +1,11 @@
+class CreateStudyRecords < ActiveRecord::Migration[6.1]
+  def change
+    create_table :study_records do |t|
+      t.string :title
+      t.text :body
+      t.integer :num_of_session
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
